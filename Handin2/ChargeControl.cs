054cd2ead48fe3b85e3374300b67eed98b982894
LukasM <au617683@post.au.dk>
2022-03-17 12:48:58 +0100

    Add ChargeControl

--- Handin2/ChargeControl.cs
+++ Handin2/ChargeControl.cs
@@ -1,6 +1,33 @@
 ﻿namespace Handin2;
 
-public class ChargeControl
+public class ChargeControl : IChargeControl
 {
+    public bool Connected { get; set; }
+    private bool Charging { get; set; }
     
+    public void StartCharge()
+    {
+        if (!Connected)
+        {
+            Console.WriteLine("[ChargeControl]: Tried to charge, but phone was disconnected");
+            return;
+        }
+
+        Charging = true;
+        Console.WriteLine("[ChargeControl]: Charging started");
+    }
+
+    public void StopCharge()
+    {
+        Charging = false;
+     
+        if (!Connected)
+        {
+            Console.WriteLine("[ChargeControl]: Phone is disconnected, charging stopped");
+            return;
+        }
+        
+        Console.WriteLine("[ChargeControl]: Charging stopped");
+
+    }
 }
---
+++ Handin2/IChargeControl.cs
@@ -0,0 +1,9 @@
+﻿namespace Handin2;
+
+public interface IChargeControl
+{
+    public bool Connected { get; set; }
+
+    public void StartCharge();
+    public void StopCharge();
+}
