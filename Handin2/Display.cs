e18d341757a49bfb6d148f7dc3e8531b94625e5f
LukasM <au617683@post.au.dk>
2022-03-17 13:28:36 +0100

    Add Display with messages

--- Handin2/Display.cs
+++ Handin2/Display.cs
@@ -2,5 +2,62 @@
 
 public class Display
 {
+    private string _instructionArea = "";
+    private string _statusArea = "";
+
+    public Display() {}
+
+    #region Instructions
+    public void ShowConnectPhone()
+    {
+        _instructionArea = "Tilslut telefon";
+        _statusArea = "";
+    }
     
+    public void ShowRfidInstruction()
+    {
+        _instructionArea = "Indlæs RFID";
+        _statusArea = "";
+    }
+    
+    public void ShowOccupied()
+    {
+        _instructionArea = "Ladeskabet er optaget";
+        _statusArea = "";
+    }
+    #endregion
+    
+    #region Errors
+    public void ShowConnectionError()
+    {
+        _instructionArea = "Tilslutningsfejl";
+        _statusArea = "";
+    }
+
+    public void ShowRfidError()
+    {
+        _instructionArea = "RFID fejl";
+        _statusArea = "";
+    }
+    #endregion
+
+    #region Charging
+    public void ShowCharging()
+    {
+        _instructionArea = "";
+        _statusArea = "Telefonen lader...";
+    }
+
+    public void ShowFullyCharged()
+    {
+        _instructionArea = "";
+        _statusArea = "Telefonen er fuldt opladt";
+    }
+
+    public void ShowChargeError()
+    {
+        _instructionArea = "";
+        _statusArea = "Der er sket en fejl, fjern telefon";
+    }
+    #endregion
 }
