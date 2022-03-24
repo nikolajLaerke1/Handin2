1e6480f3326c2bbe9db82f9479d5c409dd4bf694
LukasM <au617683@post.au.dk>
2022-03-24 15:56:39 +0100

    Fix test formatting

--- Handin_2.test.unit/TestRfidReader.cs
+++ Handin_2.test.unit/TestRfidReader.cs
@@ -1,40 +1,34 @@
-﻿using System;
-using NSubstitute;
+﻿using Handin2;
 using NUnit.Framework;
 
-namespace Handin2.test.unit
+namespace Handin_2.test.unit;
+
+[TestFixture]
+public class TestRfidReader
 {
+    private IRfidReader _uut;
+    private RfidEventArgs _receivedEventArgs;
 
-
-    [TestFixture]
-    public class TestRfidReader
+    [SetUp]
+    public void Setup()
     {
-        private IRfidReader _uut;
-        private RfidEventArgs _receivedEventArgs;
+        _uut = new RfidReader();
+        _receivedEventArgs = null;
 
-        [SetUp]
-        public void Setup()
-        {
-            _uut = new RfidReader();
-            _receivedEventArgs = null;
+        _uut.RfidEvent +=
+            (o, args) => { _receivedEventArgs = args; };
+    }
 
-            _uut.RfidEvent +=
-                (o, args) => { _receivedEventArgs = args; };
-            
-
-        }
-
-        [Test]
-        public void Rfid_EventRaisedWith1_EventRaisedWith1()
-        {
-            var wasCalled = false;
+    [Test]
+    public void Rfid_EventRaisedWith1_EventRaisedWith1()
+    {
+        var wasCalled = false;
 
 
-            _uut.RfidEvent += (_, _) => wasCalled = true;
-            _uut.OnRfidRead(1);
+        _uut.RfidEvent += (_, _) => wasCalled = true;
+        _uut.OnRfidRead(1);
 
-            Assert.True(wasCalled);
-            Assert.That(_receivedEventArgs.Id, Is.EqualTo(1));
-        }
+        Assert.True(wasCalled);
+        Assert.That(_receivedEventArgs.Id, Is.EqualTo(1));
     }
 }
--- Handin_2.test.unit/TestStationControl.cs
+++ Handin_2.test.unit/TestStationControl.cs
@@ -3,29 +3,27 @@ using Ladeskab;
 using NSubstitute;
 using NUnit.Framework;
 
-namespace Handin_2.test.unit
+namespace Handin_2.test.unit;
+
+[TestFixture]
+public class TestStationControl
 {
+    private StationControl _uut;
+    private IChargeControl fakeChargeControl;
+    private IDoor fakeDoor;
+    private IDisplay fakeDisplay;
+    private IRfidReader fakeReader;
 
-    [TestFixture]
-    public class TestStationControl
+    [SetUp]
+    public void Setup()
     {
-        private StationControl _uut;
-        private IChargeControl fakeChargeControl;
-        private IDoor fakeDoor;
-        private IDisplay fakeDisplay;
-        private IRfidReader fakeReader;
 
-        [SetUp]
-        public void Setup()
-        {
+        fakeChargeControl = Substitute.For<ChargeControl>();
+        fakeDoor = Substitute.For<Door>();
+        fakeDisplay = Substitute.For<Display>();
+        fakeReader = Substitute.For<RfidReader>();
 
-            fakeChargeControl = Substitute.For<ChargeControl>();
-            fakeDoor = Substitute.For<Door>();
-            fakeDisplay = Substitute.For<Display>();
-            fakeReader = Substitute.For<RfidReader>();
+        _uut = new StationControl(fakeChargeControl, fakeDoor, fakeDisplay, fakeReader);
 
-            _uut = new StationControl(fakeChargeControl, fakeDoor, fakeDisplay, fakeReader);
-
-        }
     }
 }
