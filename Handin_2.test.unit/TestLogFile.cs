3cc2255a5100ddd0b31aeba549c0814795581f54
s-bogelund <simbj94@gmail.com>
2022-03-24 16:28:06 +0100

    LogFile 100p covered

--- Handin_2.test.unit/TestLogFile.cs
+++ Handin_2.test.unit/TestLogFile.cs
@@ -21,10 +21,28 @@ public class TestLogFile
 	}
 
 	[Test]
-	public bool LogDoorLocked_NoFile_NewFileCreated()
+	public void LogDoorLocked_NoFile_NewFileCreated()
 	{
 		_uut.LogDoorLocked(0);
+
+		Assert.True(File.Exists(testPath));
+
+	}
+	
+	[Test]
+	public void LogDoorUnLocked_NoFile_NewFileCreated()
+	{
+		_uut.LogDoorUnlocked(0);
+
+		Assert.True(File.Exists(testPath));
+
+	}
+
+	[Test]
+	public void LogFileCtor_NoObjectExists_ObjectCreated()
+	{
+		LogFile uut2 = new();
 		
-		
+		Assert.IsNotNull(uut2);
 	}
 }
