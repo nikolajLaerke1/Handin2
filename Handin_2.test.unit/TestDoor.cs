cfbbcfc1c9d86f4740b133e41855748fa7e8d44a
LukasM <au617683@post.au.dk>
2022-03-25 12:50:29 +0100

    Add additional test coverage to Door

--- Handin_2.test.unit/TestDoor.cs
+++ Handin_2.test.unit/TestDoor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Castle.Core.Internal;
 using NUnit.Framework;
 
 namespace Handin2.test.unit
@@ -56,8 +57,29 @@ namespace Handin2.test.unit
             _uut.OnDoorClose();
             Assert.That(_receivedEventArgs.NewState, Is.EqualTo("closed"));
         }
+        
+        [TestCase(true, true)]
+        [TestCase(false, true)]
+        public void LockDoor_DoorInitialized_LockedUpdated(bool initialLockState, bool expectedLockState)
+        {
+            _uut.Locked = initialLockState;
+            
+            _uut.LockDoor();
+            
+            Assert.That(_uut.Locked, Is.EqualTo(expectedLockState));
+        }
 
-
+        [TestCase(true, false)]
+        [TestCase(false, false)]
+        public void UnlockDoor_DoorInitialized_LockedUpdated(bool initialLockState, bool expectedLockState)
+        {
+            _uut.Locked = initialLockState;
+            
+            _uut.UnlockDoor();
+            
+            Assert.That(_uut.Locked, Is.EqualTo(expectedLockState));
+        }
+        
     }
 
     
